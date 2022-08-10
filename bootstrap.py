cf69f25d8f90abfd167ee6b70a92d87d05831c74
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2022-08-10 13:21:23 +0100

    adapt to new templates
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@burendo.com>

--- bootstrap.py
+++ bootstrap.py
@@ -13,27 +13,16 @@ from dateutil.tz import tzlocal
 
 def main():
     if "AWS_PROFILE" in os.environ:
-        boto3.setup_default_session(profile_name=os.environ["AWS_PROFILE"])
-    if "AWS_PROFILE_MGT_DEV" in os.environ:
-        secrets_session = boto3.Session(profile_name=os.environ["AWS_PROFILE_MGT_DEV"])
+        secrets_manager = boto3.client("secretsmanager")
     elif "AWS_SECRETS_ROLE" in os.environ:
         secrets_session = assumed_role_session(os.environ["AWS_SECRETS_ROLE"])
-    if "AWS_REGION" in os.environ:
-        secrets_manager = secrets_session.client(
-            "secretsmanager", region_name=os.environ["AWS_REGION"]
-        )
     else:
         secrets_manager = secrets_session.client("secretsmanager")
 
     try:
         terraform_secret = secrets_manager.get_secret_value(
-            SecretId="/burendo/terraform")
-        burendo_secret = secrets_manager.get_secret_value(
-            SecretId="/burendo/burendo-secrets"
-        )
-        burendo_sensitive = secrets_manager.get_secret_value(
-            SecretId="/burendo/burendo"
-        )
+            SecretId="burendo-terraform-secrets")
+       
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
@@ -46,21 +35,18 @@ def main():
 
     config_data = yaml.load(terraform_secret['SecretBinary'], Loader=yaml.FullLoader)
     config_data['terraform'] = json.loads(terraform_secret['SecretBinary'])["terraform"]
-    config_data['github_username'] = json.loads(burendo_sensitive['SecretBinary'])["burendo_github_username"]
-    config_data['github_email'] = json.loads(burendo_sensitive['SecretBinary'])["burendo_github_email"]
-    config_data['github_token'] = json.loads(burendo_secret['SecretBinary'])["burendo_github_pat"]
-    config_data['github_webhook_token'] = json.loads(burendo_sensitive['SecretBinary'])["burendo_github_webhook_token"]
-    config_data['dockerhub_username'] = json.loads(burendo_sensitive['SecretBinary'])["dockerhub_username"]
-    config_data['dockerhub_password'] = json.loads(burendo_secret['SecretBinary'])["dockerhub_token"]
-    config_data['terraform_version'] = json.loads(burendo_sensitive['SecretBinary'])["terraform_version"]
 
     with open("terraform.tf.j2") as in_template:
         template = jinja2.Template(in_template.read())
     with open("terraform.tf", "w+") as terraform_tf:
         terraform_tf.write(template.render(config_data))
-    with open("terraform.tfvars.j2") as in_template:
+    with open("variables.tf.j2") as in_template:
         template = jinja2.Template(in_template.read())
-    with open("terraform.tfvars", "w+") as terraform_tf:
+    with open("variables.tf", "w+") as terraform_tf:
+        terraform_tf.write(template.render(config_data))
+    with open("locals.tf.j2") as in_template:
+        template = jinja2.Template(in_template.read())
+    with open("locals.tf", "w+") as terraform_tf:
         terraform_tf.write(template.render(config_data))
     print("Terraform config successfully created")
 
@@ -87,3 +73,4 @@ def assumed_role_session(role_arn: str, base_session: botocore.session.Session =
 
 if __name__ == "__main__":
     main()
+    
