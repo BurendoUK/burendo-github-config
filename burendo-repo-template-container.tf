fb3ddd724d14e04f200f3944d7cb1f2cf8f084a2
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2022-08-12 09:20:49 +0100

    Add Burendo Container template repo
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@burendo.com>

---
+++ burendo-repo-template-container.tf
@@ -0,0 +1,67 @@
+resource "github_repository" "burendo_repo_template_container" {
+  name        = "burendo-repo-template-container"
+  description = "burendo-repo-template-container"
+  auto_init   = false
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+  topics                 = local.common_topics
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner      = var.github_organization
+    repository = "burendo-repo-template"
+  }
+}
+
+resource "github_team_repository" "burendo_repo_template_container_burendo" {
+  repository = github_repository.burendo_repo_template_container.name
+  permission = "push"
+}
+
+resource "github_team_repository" "burendo_repo_template_container-admin" {
+  repository = github_repository.burendo_repo_template_container.name
+  team_id    = github_team.engineering.id
+  permission = "admin"
+}
+
+resource "github_branch_protection" "burendo_repo_template_container_master" {
+  branch         = github_repository.burendo_repo_template_container.default_branch
+  repository     = github_repository.burendo_repo_template_container.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_issue_label" "burendo_repo_template_container" {
+  for_each   = { for common_label in local.common_labels : common_label.name => common_label }
+  color      = each.value.colour
+  name       = each.value.name
+  repository = github_repository.burendo_repo_template_container.name
+}
+
+resource "null_resource" "burendo_repo_template_container" {
+  triggers = {
+    repo = github_repository.burendo_repo_template_container.name
+  }
+  provisioner "local-exec" {
+    command = "./initial-commit.sh ${github_repository.burendo_repo_template_container.name} '${github_repository.burendo_repo_template_container.description}' ${github_repository.burendo_repo_template_container.template.0.repository}"
+  }
+}
+
+resource "github_actions_secret" "burendo_repo_template_container_terraform_version" {
+  repository      = github_repository.burendo_repo_template_container.name
+  secret_name     = "TERRAFORM_VERSION"
+  plaintext_value = var.terraform_version
+}
