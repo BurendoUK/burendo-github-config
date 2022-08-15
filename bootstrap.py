d86dc6a03b943606c97ef095691006ba1bf222f2
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2022-08-15 14:48:10 +0100

    Create GitHub actions (#6)
    * Add GitHub Secrets for pipelines
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add AWS key pairs for pipelines
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Create aws key pair vars for pipelines
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Create github actions
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * trigger
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Fix var ref
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * fix call
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Fix call
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add action to get creds and assume role
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Use boto3 client for GHA
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Rename action
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Skip tagging until I sort out IAM
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add role duration of 15 mins
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Try assume role func
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * udpate
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add credential test
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * test some more
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * test
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * tf tests
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Mirror changes to merge profile
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * test log redaction
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * restore fail clause, rename secret
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add no-clor flag
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Add else to fail
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * Update bootstrap.py
    
    Co-authored-by: steveburtonBUR <106547073+steveburtonBUR@users.noreply.github.com>
    
    * removed unused function
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    * refine the if pattern
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>
    Co-authored-by: steveburtonBUR <106547073+steveburtonBUR@users.noreply.github.com>

--- bootstrap.py
+++ bootstrap.py
@@ -12,12 +12,10 @@ from dateutil.tz import tzlocal
 
 
 def main():
-    if "AWS_PROFILE" in os.environ:
-        secrets_manager = boto3.client("secretsmanager")
-    elif "AWS_SECRETS_ROLE" in os.environ:
-        secrets_session = assumed_role_session(os.environ["AWS_SECRETS_ROLE"])
-    else:
-        secrets_manager = secrets_session.client("secretsmanager")
+    if not ("AWS_PROFILE" in os.environ or "AWS_SECRETS_ROLE" in os.environ):
+        print("ERROR: Missing environment variables. Must contain either AWS_PROFILE (local) or AWS_SECRETS_ROLE (GHA)")
+
+    secrets_manager = boto3.client("secretsmanager")
 
     try:
         terraform_secret = secrets_manager.get_secret_value(
@@ -43,7 +41,7 @@ def main():
         template = jinja2.Template(in_template.read())
     with open("terraform.tf", "w+") as terraform_tf:
         terraform_tf.write(template.render(config_data))
-    with open("terraform.tfvars.j2") as in_template:
+    with open("terraform.tfvars.j2") as in_template: 
         template = jinja2.Template(in_template.read())
     with open("terraform.tfvars", "w+") as terraform_tf:
         terraform_tf.write(template.render(config_data))
@@ -56,27 +54,6 @@ def main():
     with open("locals.tf", "w+") as terraform_tf:
         terraform_tf.write(template.render(config_data))
     print("Terraform config successfully created")
-
-
-def assumed_role_session(role_arn: str, base_session: botocore.session.Session = None):
-    base_session = base_session or boto3.session.Session()._session
-    fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
-        client_creator=base_session.create_client,
-        source_credentials=base_session.get_credentials(),
-        role_arn=role_arn,
-        extra_args={
-            #    'RoleSessionName': None # set this if you want something non-default
-        },
-    )
-    creds = botocore.credentials.DeferredRefreshableCredentials(
-        method="assume-role",
-        refresh_using=fetcher.fetch_credentials,
-        time_fetcher=lambda: datetime.datetime.now(tzlocal()),
-    )
-    botocore_session = botocore.session.Session()
-    botocore_session._credentials = creds
-    return boto3.Session(botocore_session=botocore_session)
-
 
 if __name__ == "__main__":
     main()
