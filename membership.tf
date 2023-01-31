c0778a06b94a48e38974d273a41ea78df7f3fbe0
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2023-01-31 12:49:27 +0000

    Fix users and team membership (#58)
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ membership.tf
@@ -0,0 +1,83 @@
+resource "github_team_membership" "engineering_membership_ChrisScottThomas" {
+  team_id  = github_team.engineering.id
+  username = "ChrisScottThomas"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "engineering_membership_steveburtonBUR" {
+  team_id  = github_team.engineering.id
+  username = "steveburtonBUR"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "engineering_membership_kevinkuszyk" {
+  team_id  = github_team.engineering.id
+  username = "kevinkuszyk"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "engineering_membership_nomad3k" {
+  team_id  = github_team.engineering.id
+  username = "nomad3k"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_phillip-stanley" {
+  team_id  = github_team.engineering.id
+  username = "phillip-stanley"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_connoravo" {
+  team_id  = github_team.engineering.id
+  username = "connoravo"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_zesh92" {
+  team_id  = github_team.engineering.id
+  username = "zesh92"
+  role     = "member"
+}
+
+resource "github_team_membership" "automation_membership_burendoci" {
+  team_id  = github_team.automation.id
+  username = "burendoci"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "engineering_membership_brianbayes" {
+  team_id  = github_team.engineering.id
+  username = "brianbayes"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_rohoolio" {
+  team_id  = github_team.engineering.id
+  username = "rohoolio"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_burpaulbutler" {
+  team_id  = github_team.engineering.id
+  username = "BurPaulButler"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "engineering_membership_garygreenburendo" {
+  team_id  = github_team.engineering.id
+  username = "garygreenburendo"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_luketowell" {
+  team_id  = github_team.engineering.id
+  username = "luketowell"
+  role     = "member"
+}
+
+resource "github_team_membership" "engineering_membership_jordierochburendo" {
+  team_id  = github_team.burendo.id
+  username = "JordieRochBurendo"
+  role     = "member"
+}
