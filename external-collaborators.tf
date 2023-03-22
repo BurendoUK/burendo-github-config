ef422127bfde68409ee85fe4ea6ae954e970303c
Steven Burton <steven.burton@burendo.com>
2023-03-22 12:05:19 +0000

    Fix

--- external-collaborators.tf
+++ external-collaborators.tf
@@ -3,17 +3,16 @@
 */
 
 locals {
-    handbook_collaborators_github_usernames = 
-    toset([   #For a new handbook collaborator, simply add GH username to this array
-        "DaveHBurendo",
-        "JarvBurendo",
-    ])
+  handbook_collaborators_github_usernames = toset([ #For a new handbook collaborator, simply add GH username to this array
+    "DaveHBurendo",
+    "JarvBurendo",
+  ])
 }
 
 resource "github_repository_collaborator" "external_collaborator_burendo_handbook_public" {
   for_each   = local.handbook_collaborators_github_usernames
   repository = "burendo-handbook-public"
-  username   = "${each.key}"
+  username   = each.key
   permission = "push"
 }
 
