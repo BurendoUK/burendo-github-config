c3cb809748d31e82ed920da7dc20b1224e28afb4
Connor Avery <55276424+connoravo@users.noreply.github.com>
2023-04-03 09:55:14 +0100

    Secrets
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- burendo-website-infrastructure.tf
+++ burendo-website-infrastructure.tf
@@ -1,8 +1,8 @@
 resource "github_repository" "burendo_website_infrastructure" {
-  name             = "burendo-website-infrastructure"
-  description      = "The burendo dot com website infrastructure. The content exists in burendouk/burendo-website"
-  visibility       = "public"
-  auto_init        = false
+  name        = "burendo-website-infrastructure"
+  description = "The burendo dot com website infrastructure. The content exists in burendouk/burendo-website"
+  visibility  = "public"
+  auto_init   = false
 
   allow_merge_commit     = false
   delete_branch_on_merge = true
@@ -14,7 +14,7 @@ resource "github_repository" "burendo_website_infrastructure" {
   }
 
   template {
-    owner = var.github_org
+    owner      = var.github_org
     repository = "burendo-repo-template-terraform"
   }
 }
@@ -33,7 +33,7 @@ resource "github_team_repository" "burendo_website_infrastructure_admin" {
 
 resource "github_branch_protection" "burendo_website_infrastructure_main" {
   pattern        = github_repository.burendo_website_infrastructure.default_branch
-  repository_id     = github_repository.burendo_website_infrastructure.name
+  repository_id  = github_repository.burendo_website_infrastructure.name
   enforce_admins = false
 
   required_status_checks {
@@ -51,4 +51,40 @@ resource "github_issue_label" "burendo_website_infrastructure" {
   color      = each.value.colour
   name       = each.value.name
   repository = github_repository.burendo_website_infrastructure.name
+}
+
+resource "github_actions_secret" "aws_access_key_id_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "ACTIONS_ACCESS_KEY_ID"
+  plaintext_value = var.gha_aws.access_key_id
+}
+
+resource "github_actions_secret" "aws_secret_access_key_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "ACTIONS_SECRET_ACCESS_KEY"
+  plaintext_value = var.gha_aws.secret_access_key
+}
+
+resource "github_actions_secret" "aws_role_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "AWS_GHA_ROLE"
+  plaintext_value = "arn:aws:iam::${local.account["burendo-prod"]}:role/ci"
+}
+
+resource "github_actions_secret" "dev_aws_role_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "AWS_GHA_ROLE_DEV"
+  plaintext_value = "arn:aws:iam::${local.account["burendo-dev"]}:role/ci"
+}
+
+resource "github_actions_secret" "aws_terraform_version_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "TERRAFORM_VERSION"
+  plaintext_value = var.terraform_version
+}
+
+resource "github_actions_secret" "aws_acc_prod_burendo_website_infrastructure" {
+  repository      = github_repository.burendo_website_infrastructure.name
+  secret_name     = "AWS_GHA_ACC_PROD"
+  plaintext_value = local.account["burendo-prod"]
 }
