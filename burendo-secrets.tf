e7ab303c209f38ea55f8037e3744caa41e55b8d0
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2023-04-21 08:34:43 +0100

    Create private repo to hold secrets (#78)
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ burendo-secrets.tf
@@ -0,0 +1,54 @@
+resource "github_repository" "burendo_secrets" {
+  name        = "burendo-secrets"
+  description = "A Private repository holding secrets content stored in AWS Secrets Manager"
+  visibility  = "private"
+  auto_init   = false
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+  topics                 = local.common_topics
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner      = var.github_org
+    repository = "burendo-repo-template"
+  }
+}
+
+resource "github_team_repository" "burendo_secrets_burendo" {
+  repository = github_repository.burendo_secrets.name
+  team_id    = github_team.burendo.id
+  permission = "push"
+}
+
+resource "github_team_repository" "burendo_secrets_admin" {
+  repository = github_repository.burendo_secrets.name
+  team_id    = github_team.engineering.id
+  permission = "admin"
+}
+
+resource "github_branch_protection" "burendo_secrets_main" {
+  pattern        = github_repository.burendo_secrets.default_branch
+  repository_id  = github_repository.burendo_secrets.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_issue_label" "burendo_secrets" {
+  for_each   = { for common_label in local.common_labels : common_label.name => common_label }
+  color      = each.value.colour
+  name       = each.value.name
+  repository = github_repository.burendo_secrets.name
+}
