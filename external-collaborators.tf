fb7580472d731f86d1fa90b143d970ff02af0955
Steven Burton <steven.burton@burendo.com>
2023-08-10 16:55:46 +0100

    Comma

--- external-collaborators.tf
+++ external-collaborators.tf
@@ -9,8 +9,8 @@ locals {
     "HBoulton20", #Heather Bolton
     "paddylemons1",
     "Bex26", #Rebecca Shaw
-    "wozzaRendo" #Warran Mav
-    "BurendoKate" #Kate Smith
+    "wozzaRendo", #Warran Mav
+    "BurendoKate", #Kate Smith
   ])
 
   handbook_private_collaborators_github_usernames = toset([
