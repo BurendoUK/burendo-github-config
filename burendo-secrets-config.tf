8e7b6fad9b734a3b61c0f9b08e697dbde3db891b
ChrisScottThomas <chris.scott-thomas@burendo.com>
2024-04-02 09:35:11 +0000

    fix secret name for secrets config
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- burendo-secrets-config.tf
+++ burendo-secrets-config.tf
@@ -73,7 +73,7 @@ resource "github_actions_secret" "aws_secret_access_key_burendo_secrets_config" 
 
 resource "github_actions_secret" "aws_role_burendo_secrets_config" {
   repository      = github_repository.burendo_secrets_config.name
-  secret_name     = "AWS_GHA_ROLE"
+  secret_name     = "AWS_GHA_ROLE_PROD"
   plaintext_value = "arn:aws:iam::${local.account["burendo-prod"]}:role/ci"
 }
 
