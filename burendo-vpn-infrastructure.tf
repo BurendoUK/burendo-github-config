a135ae0ef8e6b454a9f949e5ab01921963e2fd4b
Connor Avery <55276424+connoravo@users.noreply.github.com>
2024-10-28 15:43:45 +0000

    Update enforce_admin option
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- burendo-vpn-infrastructure.tf
+++ burendo-vpn-infrastructure.tf
@@ -34,7 +34,7 @@ resource "github_team_repository" "burendo_vpn_admin" {
 resource "github_branch_protection" "burendo_vpn_main" {
   pattern        = github_repository.burendo_vpn.default_branch
   repository_id  = github_repository.burendo_vpn.name
-  enforce_admins = false
+  enforce_admins = true
 
   required_status_checks {
     strict = true
