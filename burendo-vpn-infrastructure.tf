a77e61d9611f16fc1e518a3f29a4d3a574d7b4f3
Connor Avery <55276424+connoravo@users.noreply.github.com>
2024-10-30 08:31:28 +0000

    Use repo code and not template code
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- burendo-vpn-infrastructure.tf
+++ burendo-vpn-infrastructure.tf
@@ -1,39 +1,39 @@
-resource "github_repository" "burendo_vpn" {
-  name        = "burendo-vpn-infrastructure"
-  description = "Burendo VPN"
-  visibility  = "private"
-  auto_init   = false
+resource "github_repository" "burendo_vpn_infrastructure" {
+  name             = "burendo-vpn-infrastructure"
+  description      = "Burendo VPN Infrastructure"
+  visibility       = "public"
+  auto_init        = false
 
   allow_merge_commit     = false
   delete_branch_on_merge = true
   has_issues             = true
-  topics                 = concat(local.common_topics, local.aws_topics)
+  topics                 = local.common_topics
 
   lifecycle {
     prevent_destroy = true
   }
 
   template {
-    owner      = var.github_org
-    repository = "burendo-vpn-infrastructure"
+    owner = var.github_org
+    repository = "burendo-repo-template"
   }
 }
 
-resource "github_team_repository" "burendo_vpn_burendo" {
-  repository = github_repository.burendo_vpn.name
+resource "github_team_repository" "burendo_vpn_infrastructure_burendo" {
+  repository = github_repository.burendo_vpn_infrastructure.name
   team_id    = github_team.burendo.id
   permission = "push"
 }
 
-resource "github_team_repository" "burendo_vpn_admin" {
-  repository = github_repository.burendo_vpn.name
+resource "github_team_repository" "burendo_vpn_infrastructure_admin" {
+  repository = github_repository.burendo_vpn_infrastructure.name
   team_id    = github_team.engineering.id
   permission = "admin"
 }
 
-resource "github_branch_protection" "burendo_vpn_main" {
-  pattern        = github_repository.burendo_vpn.default_branch
-  repository_id  = github_repository.burendo_vpn.name
+resource "github_branch_protection" "burendo_vpn_infrastructure_main" {
+  pattern        = github_repository.burendo_vpn_infrastructure.default_branch
+  repository_id     = github_repository.burendo_vpn_infrastructure.name
   enforce_admins = true
 
   required_status_checks {
@@ -46,9 +46,9 @@ resource "github_branch_protection" "burendo_vpn_main" {
   }
 }
 
-resource "github_issue_label" "burendo_vpn" {
+resource "github_issue_label" "burendo_vpn_infrastructure" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.burendo_vpn.name
+  repository = github_repository.burendo_vpn_infrastructure.name
 }
