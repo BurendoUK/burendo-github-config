5b1d12ab6b2caf36b7cb3d2c22e2dc07903a094d
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-03-04 11:50:08 +0000

    Create db repo for the handbook
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

---
+++ burendo-handbook-db.tf
@@ -0,0 +1,62 @@
+resource "github_repository" "burendo_handbook_db" {
+  name        = "burendo-handbook-db"
+  description = "burendo-handbook-db"
+  visibility  = "public"
+  auto_init   = false
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+  topics                 = local.common_topics
+
+  is_template = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner      = var.github_org
+    repository = "burendo-repo-template"
+  }
+}
+
+resource "github_team_repository" "burendo_handbook_db_burendo" {
+  repository = github_repository.burendo_handbook_db.name
+  team_id    = github_team.burendo.id
+  permission = "push"
+}
+
+resource "github_team_repository" "burendo_handbook_db_admin" {
+  repository = github_repository.burendo_handbook_db.name
+  team_id    = github_team.engineering.id
+  permission = "admin"
+}
+
+resource "github_branch_protection" "burendo_handbook_db_main" {
+  pattern        = github_repository.burendo_handbook_db.default_branch
+  repository_id  = github_repository.burendo_handbook_db.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_issue_label" "burendo_handbook_db" {
+  for_each   = { for common_label in local.common_labels : common_label.name => common_label }
+  color      = each.value.colour
+  name       = each.value.name
+  repository = github_repository.burendo_handbook_db.name
+}
+
+resource "github_actions_secret" "burendo_handbook_db_terraform_version" {
+  repository      = github_repository.burendo_handbook_db.name
+  secret_name     = "TERRAFORM_VERSION"
+  plaintext_value = var.terraform_version
+}
