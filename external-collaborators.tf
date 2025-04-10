a2e33d27dbc750381f0f99bb83ed8d075b80fa43
Chris Scott-Thomas <chris.scott-thomas@burendo.com>
2025-04-10 11:09:07 +0100

    Add Robyn as Handbook collaborator (#125)
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- external-collaborators.tf
+++ external-collaborators.tf
@@ -4,12 +4,13 @@
 
 locals {
   handbook_public_collaborators_github_usernames = toset([
-    "waljj1",      #Paul Glover
-    "HBoulton20",  #Heather Bolton
-    "warranmav",   #Warran Mav
-    "BurendoKate", #Kate Smith
-    "jrsscott",    #James Scott
-    "rhrh19",      #Richard Hardicre
+    "waljj1",        #Paul Glover
+    "HBoulton20",    #Heather Bolton
+    "warranmav",     #Warran Mav
+    "BurendoKate",   #Kate Smith
+    "jrsscott",      #James Scott
+    "rhrh19",        #Richard Hardicre
+    "RobynBrooke25", #Robyn Brooke
   ])
 
   handbook_private_collaborators_github_usernames = toset([
