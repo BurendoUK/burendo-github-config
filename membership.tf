e83c2008f9899f1aa4f9e0c4c83c073494b4492b
ChrisScottThomas <chris.scott-thomas@burendo.com>
2025-05-22 10:47:04 +0000

    Add Chris Ramsden to the Org
    Signed-off-by: ChrisScottThomas <chris.scott-thomas@burendo.com>

--- membership.tf
+++ membership.tf
@@ -116,3 +116,9 @@ resource "github_team_membership" "engineering_membership_sam_aki" {
   username = "sam-aki"
   role     = "member"
 }
+
+resource "github_team_membership" "engineering_membership_chris_ramsden" {
+  team_id  = github_team.engineering.id
+  username = "tvking"
+  role     = "member"
+}
