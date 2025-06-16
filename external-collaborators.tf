61ccf92f3f662f1081695dff525e62e680030afd
Chris Ramsden <tvking@users.noreply.github.com>
2025-06-16 16:39:10 +0100

    Add Afaan to the data dashboard repos (#137)
    Co-authored-by: Chris Ramsden <chris.ramsden@burendo.com>

--- external-collaborators.tf
+++ external-collaborators.tf
@@ -20,6 +20,10 @@ locals {
   oddfellows_private_collaborators_github_usernames = toset([
     "simonf-burendo", # Simon Futter Contractor
   ])
+
+  data_dashboard_public_collaborators_github_usernames = toset([
+    "A-Ashiq",        #Afaan Ashiq
+  ])
 }
 
 resource "github_repository_collaborator" "external_collaborator_burendo_handbook_public" {
@@ -43,6 +47,27 @@ resource "github_repository_collaborator" "external_collaborator_burendo_handboo
 resource "github_repository_collaborator" "external_collaborator_burendo_oddfellows_pt_frontend" {
   for_each   = local.oddfellows_private_collaborators_github_usernames
   repository = "burendo-oddfellows-pt-frontend"
+  username   = each.key
+  permission = "push"
+}
+
+resource "github_repository_collaborator" "external_collaborator_data_dashboard_api_public" {
+  for_each   = local.data_dashboard_public_collaborators_github_usernames
+  repository = "data-dashboard-api"
+  username   = each.key
+  permission = "push"
+}
+
+resource "github_repository_collaborator" "external_collaborator_data_dashboard_frontend_public" {
+  for_each   = local.data_dashboard_public_collaborators_github_usernames
+  repository = "data-dashboard-frontend"
+  username   = each.key
+  permission = "push"
+}
+
+resource "github_repository_collaborator" "external_collaborator_data_dashboard_infra_public" {
+  for_each   = local.data_dashboard_public_collaborators_github_usernames
+  repository = "data-dashboard-infra"
   username   = each.key
   permission = "push"
 }
