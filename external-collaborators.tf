2bed7c37c6f30e6de1d37f79c7518699261d8b65
Chris Ramsden <chris.ramsden@burendo.com>
2025-06-16 18:52:32 +0100

    Fix repo name with Burendo pre-pend

--- external-collaborators.tf
+++ external-collaborators.tf
@@ -72,9 +72,9 @@ resource "github_repository_collaborator" "external_collaborator_data_dashboard_
   permission = "push"
 }
 
-resource "github_repository_collaborator" "external_collaborator_nhs_dashboard_tooling_private" {
+resource "github_repository_collaborator" "external_collaborator_burendo_nhs_dashboard_tooling_private" {
   for_each   = local.data_dashboard_public_collaborators_github_usernames
-  repository = "nhs-dashboard-tooling"
+  repository = "burendo-nhs-dashboard-tooling"
   username   = each.key
   permission = "push"
 }
