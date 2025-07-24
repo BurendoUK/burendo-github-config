1aa1daa69173049e52c173e272709814f12ac542
Mathew Munro <mathew.munro@burendo.com>
2025-07-24 16:04:55 +0100

    change terraform names to underscore insted of dash

--- safeshout-infra.tf
+++ safeshout-infra.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "safeshout-infra" {
+resource "github_repository" "safeshout_infra" {
   name             = "safeshout-infra"
   description      = "safeshout-infra - Repo for SafeShout Infrastructure - AWS - Terraform "
   visibility       = "private"
@@ -20,19 +20,19 @@ resource "github_repository" "safeshout-infra" {
   }
 }
 
-resource "github_team_repository" "safeshout-infra_burendo" {
+resource "github_team_repository" "safeshout_infra_burendo" {
   repository = github_repository.safeshout-infra.name
   team_id    = github_team.burendo.id
   permission = "push"
 }
 
-resource "github_team_repository" "safeshout-infra_admin" {
+resource "github_team_repository" "safeshout_infra_admin" {
   repository = github_repository.safeshout-infra.name
   team_id    = github_team.engineering.id
   permission = "admin"
 }
 
-resource "github_branch_protection" "safeshout-infra_main" {
+resource "github_branch_protection" "safeshout_infra_main" {
   pattern        = github_repository.safeshout-infra.default_branch
   repository_id     = github_repository.safeshout-infra.name
   enforce_admins = true
@@ -47,7 +47,7 @@ resource "github_branch_protection" "safeshout-infra_main" {
   }
 }
 
-resource "github_issue_label" "safeshout-infra" {
+resource "github_issue_label" "safeshout_infra" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
