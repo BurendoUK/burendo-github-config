82c252dda2a45e2285c260a0c01e8373bf1971f8
Mathew Munro <mathew.munro@burendo.com>
2025-07-24 16:12:47 +0100

    fix

--- safeshout-infra.tf
+++ safeshout-infra.tf
@@ -21,20 +21,20 @@ resource "github_repository" "safeshout_infra" {
 }
 
 resource "github_team_repository" "safeshout_infra_burendo" {
-  repository = github_repository.safeshout-infra.name
+  repository = github_repository.safeshout_infra.name
   team_id    = github_team.burendo.id
   permission = "push"
 }
 
 resource "github_team_repository" "safeshout_infra_admin" {
-  repository = github_repository.safeshout-infra.name
+  repository = github_repository.safeshout_infra.name
   team_id    = github_team.engineering.id
   permission = "admin"
 }
 
 resource "github_branch_protection" "safeshout_infra_main" {
-  pattern        = github_repository.safeshout-infra.default_branch
-  repository_id     = github_repository.safeshout-infra.name
+  pattern        = github_repository.safeshout_infra.default_branch
+  repository_id     = github_repository.safeshout_infra.name
   enforce_admins = true
 
   required_status_checks {
@@ -51,5 +51,5 @@ resource "github_issue_label" "safeshout_infra" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.safeshout-infra.name
+  repository = github_repository.safeshout_infra.name
 }
