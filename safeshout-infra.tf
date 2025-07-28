ef854e08b02d6c9737d5cf541346a02c3a93c275
mathew-munro-burendo <mathew.munro@burendo.com>
2025-07-28 10:24:47 +0100

    add aws and dev role secrets (#153)
    * add aws and dev role secrets
    
    * fix wrong repo ref

--- safeshout-infra.tf
+++ safeshout-infra.tf
@@ -53,3 +53,33 @@ resource "github_issue_label" "safeshout_infra" {
   name       = each.value.name
   repository = github_repository.safeshout_infra.name
 }
+
+resource "github_actions_secret" "aws_terraform_version_safeshout_infra" {
+  repository      = github_repository.safeshout_infra.name
+  secret_name     = "TERRAFORM_VERSION"
+  plaintext_value = var.terraform_version
+}
+
+resource "github_actions_secret" "aws_access_key_id_safeshout_infra" {
+  repository      = github_repository.safeshout_infra.name
+  secret_name     = "ACTIONS_ACCESS_KEY_ID"
+  plaintext_value = var.gha_aws.access_key_id
+}
+
+resource "github_actions_secret" "aws_secret_access_key_safeshout_infra" {
+  repository      = github_repository.safeshout_infra.name
+  secret_name     = "ACTIONS_SECRET_ACCESS_KEY"
+  plaintext_value = var.gha_aws.secret_access_key
+}
+
+resource "github_actions_secret" "aws_dev_role_safeshout_infra" {
+  repository      = github_repository.safeshout_infra.name
+  secret_name     = "AWS_GHA_ROLE_DEV"
+  plaintext_value = "arn:aws:iam::${local.account["burendo-dev"]}:role/ci"
+}
+
+resource "github_actions_secret" "aws_acc_dev_safeshout_infra" {
+  repository      = github_repository.safeshout_infra.name
+  secret_name     = "AWS_GHA_ACC_DEV"
+  plaintext_value = local.account["burendo-dev"]
+}
