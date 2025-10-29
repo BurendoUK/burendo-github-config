931c9f26e6bb3aad3f2847ac8919b8212331ec31
Chris Ramsden <chris.ramsden@burendo.com>
2025-10-29 12:10:49 +0000

    Add comments to name everyone and update number of seats

--- membership.tf
+++ membership.tf
@@ -1,147 +1,147 @@
-/* 
-NOTE: We have 15 seats available overall, if your addition goes above that
+/*
+NOTE: We have 27 seats available overall, if your addition goes above that
 you need a discussion with an admin member in charge of github first
 */
 resource "github_team_membership" "engineering_membership_ConnorAvery" {
   team_id  = github_team.engineering.id
-  username = "connoravo"
+  username = "connoravo" // Connor Avery
   role     = "maintainer"
 }
 
 resource "github_team_membership" "engineering_membership_kevinkuszyk" {
   team_id  = github_team.engineering.id
-  username = "kevinkuszyk"
+  username = "kevinkuszyk" // Kevin Kuszyk
   role     = "maintainer"
 }
 
 resource "github_team_membership" "engineering_membership_phillip-stanley" {
   team_id  = github_team.engineering.id
-  username = "phillip-stanley"
+  username = "phillip-stanley" // Phillip Stanley
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_Jita81" {
   team_id  = github_team.engineering.id
-  username = "Jita81"
+  username = "Jita81" // Paul Glover
   role     = "member"
 }
 
 resource "github_team_membership" "automation_membership_burendoci" {
   team_id  = github_team.automation.id
-  username = "burendoci"
+  username = "burendoci" // Burendo CI Bot
   role     = "maintainer"
 }
 
 resource "github_team_membership" "engineering_membership_rohoolio" {
   team_id  = github_team.engineering.id
-  username = "rohoolio"
+  username = "rohoolio" // Rowan Gill
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_luketowell" {
   team_id  = github_team.engineering.id
-  username = "luketowell"
+  username = "luketowell" // Luke Towell
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_kathryn-dale" {
   team_id  = github_team.engineering.id
-  username = "kathryn-dale"
+  username = "kathryn-dale" // Kathryn Dale
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_emily_evans" {
   team_id  = github_team.engineering.id
-  username = "emilyjevans"
+  username = "emilyjevans" // Emily Evans
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_joshua_douce" {
   team_id  = github_team.engineering.id
-  username = "JoshuaDouceBurendo"
+  username = "JoshuaDouceBurendo" // Joshua Douce
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_natasha_myers" {
   team_id  = github_team.engineering.id
-  username = "natashaburendo"
+  username = "natashaburendo" // Natasha Myers
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_ramon_rodriguez" {
   team_id  = github_team.engineering.id
-  username = "ramonrodriguez-burendo"
+  username = "ramonrodriguez-burendo"  // Ramon Rodriguez
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_mauro_sorrentino" {
   team_id  = github_team.engineering.id
-  username = "maurosorrentinoburendo"
+  username = "maurosorrentinoburendo"  // Mauro Sorrentino
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_paul_walkington" {
   team_id  = github_team.engineering.id
-  username = "paulwalkington"
+  username = "paulwalkington" // Paul Walkington
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_neil_dunlop" {
   team_id  = github_team.engineering.id
-  username = "neildunlop"
+  username = "neildunlop" // Neil Dunlop
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_sam_aki" {
   team_id  = github_team.engineering.id
-  username = "sam-aki"
+  username = "sam-aki" // Sam Aki
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_chris_ramsden" {
   team_id  = github_team.engineering.id
-  username = "tvking"
+  username = "tvking" // Chris Ramsden
   role     = "maintainer"
 }
 
 resource "github_team_membership" "engineering_membership_matt_munro" {
   team_id  = github_team.engineering.id
-  username = "mathew-munro-burendo"
+  username = "mathew-munro-burendo" // Matt Munro
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_nick_lefley" {
   team_id  = github_team.engineering.id
-  username = "NickLefleyBurendo"
+  username = "NickLefleyBurendo" // Nick Lefley
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_chris_warren" {
   team_id  = github_team.engineering.id
-  username = "jumpertester"
+  username = "jumpertester" // Chris Warren
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_steven_moxon" {
   team_id  = github_team.engineering.id
-  username = "stmo262"
+  username = "stmo262" // Steven Moxon
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_ann_barr" {
   team_id  = github_team.engineering.id
-  username = "annjbarr"
+  username = "annjbarr" // Ann Barr
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_gordon_brown" {
   team_id  = github_team.engineering.id
-  username = "gordonbrown"
+  username = "gordonbrown" // Gordon Brown
   role     = "member"
 }
 
 resource "github_team_membership" "engineering_membership_mark_matthews" {
   team_id  = github_team.engineering.id
-  username = "mark-r-m"
+  username = "mark-r-m" // Mark Matthews
   role     = "member"
 }
--- users.tf
+++ users.tf
@@ -1,19 +1,19 @@
-/* 
+/*
 NOTE: Add a comment with name if not obvious from username
  */
 
 resource "github_membership" "engineering_membership_connor_avery" {
-  username = "connoravo"
+  username = "connoravo" // Connor Avery
   role     = "admin"
 }
 
 resource "github_membership" "engineering_membership_kevinkuszyk" {
-  username = "kevinkuszyk"
+  username = "kevinkuszyk" // Kevin Kuszyk
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_phillip-stanley" {
-  username = "phillip-stanley"
+  username = "phillip-stanley" // Phillip Stanley
   role     = "member"
 }
 
@@ -23,7 +23,7 @@ resource "github_membership" "engineering_membership_Jita81" {
 }
 
 resource "github_membership" "automation_membership_burendoci" {
-  username = "burendoci"
+  username = "burendoci" // Burendo CI Bot
   role     = "admin"
 }
 
@@ -33,27 +33,27 @@ resource "github_membership" "engineering_membership_rohoolio" {
 }
 
 resource "github_membership" "engineering_membership_luketowell" {
-  username = "luketowell"
+  username = "luketowell" // Luke Towell
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_kathryn-dale" {
-  username = "kathryn-dale"
+  username = "kathryn-dale" // Kathryn Dale
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_emily_evans" {
-  username = "emilyjevans"
+  username = "emilyjevans"  // Emily Evans
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_joshua_douce" {
-  username = "JoshuaDouceBurendo"
+  username = "JoshuaDouceBurendo" // Joshua Douce
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_natasha_myers" {
-  username = "natashaburendo"
+  username = "natashaburendo" // Natasha Myers
   role     = "member"
 }
 
@@ -63,61 +63,61 @@ resource "github_membership" "engineering_membership_ramon_rodriguez" {
 }
 
 resource "github_membership" "engineering_membership_mauro_sorrentino" {
-  username = "maurosorrentinoburendo"
+  username = "maurosorrentinoburendo" // Mauro Sorrentino
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_paul_walkington" {
-  username = "paulwalkington"
+  username = "paulwalkington"  // Paul Walkington
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_neil_dunlop" {
-  username = "neildunlop"
+  username = "neildunlop" // Neil Dunlop
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_sam_aki" {
-  username = "sam-aki"
+  username = "sam-aki"  // Sam Aki
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_chris_ramsden" {
-  username = "tvking"
+  username = "tvking" // Chris Ramsden
   role     = "admin"
 }
 
 resource "github_membership" "engineering_membership_matt_munro" {
-  username = "mathew-munro-burendo"
+  username = "mathew-munro-burendo" // Matt Munro
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_nick_lefley" {
-  username = "NickLefleyBurendo"
+  username = "NickLefleyBurendo" // Nick Lefley
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_chris_warren" {
-  username = "jumpertester"
+  username = "jumpertester" // Chris Warren
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_steven_moxon" {
-  username = "stmo262"
+  username = "stmo262" // Steven Moxon
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_ann_barr" {
-  username = "annjbarr"
+  username = "annjbarr" // Ann Barr
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_gordon_brown" {
-  username = "gordonbrown"
+  username = "gordonbrown" // Gordon Brown
   role     = "member"
 }
 
 resource "github_membership" "engineering_membership_mark_matthews" {
-  username = "mark-r-m"
+  username = "mark-r-m" // Mark Matthews
   role     = "member"
 }
